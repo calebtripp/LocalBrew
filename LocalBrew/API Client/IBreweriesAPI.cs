6bf28bfb704f9bd4673cfb020267a7a38fa3f1c3
calebtripp <calebtripp@outlook.com>
2022-11-29 18:07:06 -0500

    Brewery API interface complete

--- LocalBrew/API Client/IBreweriesAPI.cs
+++ LocalBrew/API Client/IBreweriesAPI.cs
@@ -1,6 +1,10 @@
-﻿namespace LocalBrew.API_Client
+﻿using LocalBrew.Models;
+
+namespace LocalBrew.API_Client
 {
     public interface IBreweriesAPI
     {
+        public IEnumerable<Brewery> GetCityBreweries();
+        public Brewery GetBrewery(string id);
     }
 }

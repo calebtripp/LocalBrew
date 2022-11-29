9a6379d4e3590f68b7152c58afadc5976d4fca0c
calebtripp <calebtripp@outlook.com>
2022-11-29 18:02:32 -0500

    Brewery Model Complete

--- LocalBrew/Models/Brewery.cs
+++ LocalBrew/Models/Brewery.cs
@@ -2,5 +2,15 @@
 {
     public class Brewery
     {
+        public string ID { get; set; }
+        public string Name { get; set; }    
+        public string BreweryType { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }    
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string website { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
     }
 }
